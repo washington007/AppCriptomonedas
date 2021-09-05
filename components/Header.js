6a2600f89b52552a6b9837e37272f454e6468acf
washington007 <washo968@gmail.com>
2021-09-04 20:08:24 -0500

    se modifico el titulo (criptomonedas$)

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, StyleSheet, Platform } from 'react-native';
 
 const Header = () => (
-    <Text style={styles.encabezado}>Criptomoneda....</Text>
+    <Text style={styles.encabezado}>Criptomonedas$</Text>
 );
  
 const styles = StyleSheet.create({
